b6b4ab3f159793a65b28a5349a6a1468f9cbf0a7
Uilian Souza <uilian@gmail.com>
2017-09-22 14:05:32 -0300

    Fetching data from webservice.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+  state = {
+    albums: []
+  };
+
   componentWillMount() {
-    console.log('componentWillMount in AlbumList!');
-  }
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => response.json())
+      .then(data => this.setState({ albums: data }));
+//      .then(response => this.setState({ albums: response.json() }));
+   }
+
+   renderAlbums() {
+     return this.state.albums.map(album =>
+       <AlbumDetail album={album} />);
+   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
-        <Text>
-          Album List!!!
-        </Text>
+        {this.renderAlbums()}
       </View>
     );
   }
