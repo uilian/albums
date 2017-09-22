3b9f4869e1f33b593d78d13e7fe4eb0aae66cabc
Uilian Souza <uilian@gmail.com>
2017-09-22 14:47:43 -0300

    Added Card component to show items in the interface.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Card from './Card';
 
 class AlbumDetail extends Component {
 
   render() {
     return (
-      <View>
+      <Card>
           <Text>{this.props.album.title}</Text>
-      </View>
+      </Card>
     );
   }
 }
